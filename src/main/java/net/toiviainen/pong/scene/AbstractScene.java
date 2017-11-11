8f08e5bfd730f8958cc9281b8fc17c028abb5cf2
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-11-11 11:43:36 +0200

    Added the missing JavaDocs for the AbstractScene.

--- src/main/java/net/toiviainen/pong/scene/AbstractScene.java
+++ src/main/java/net/toiviainen/pong/scene/AbstractScene.java
@@ -3,12 +3,25 @@ package net.toiviainen.pong.scene;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+/**
+ * <p>
+ * A scene abstraction for all three scenes in the game.
+ * </p>
+ * <p>
+ * This abstraction allows the game to easily change between the scenes. It also
+ * provides a way to ensure that each scene contains a support for ticking the
+ * current game logic related to the scene.
+ * </p>
+ */
 public abstract class AbstractScene extends Scene {
 
 	public AbstractScene(Parent root, int width, int height) {
 		super(root, width, height);
 	}
 
+	/**
+	 * A tick function that is called on each main loop iteration.
+	 */
 	public abstract void tick();
 
 }
